b35bc287ea78dca3c5137f0a10190d87ece79641
vasireddydurga-byte <vasireddydurga@gmail.com>
2025-08-13 12:31:42 -0700

    Add files via upload

--- hello.go
+++ hello.go
@@ -3,7 +3,17 @@ import ("myproject/simplecalc"
 "fmt"
 )
 func main() {
-fmt.Println("Hello World")
+	
+	fmt.Println("Hello World")
 a,b := 6,4
 fmt.Println(simplecalc.Add(a,b))
+c, d := 10, 3
+	fmt.Println(simplecalc.Sub(c, d))
+
+	e, f := 20, 4
+	fmt.Println(simplecalc.Div(e, f))
+
+	x, y := 30, 3
+	fmt.Println(simplecalc.Mul(x, y))
+
 }
