930a7175e610946f1423e9dc76bcc2d1222221ea
vasireddydurga-byte <vasireddydurga@gmail.com>
2025-08-13 12:30:51 -0700

    Add files via upload

--- calc.go
+++ calc.go
@@ -2,4 +2,18 @@ package simplecalc
 
 func Add(a, b int) int {
 	return a + b
+
+}
+func Sub(c int, d int) int {
+	s := c - d
+	return s
+}
+
+func Div(e int, f int) int {
+	return e / f
+}
+
+func Mul(x int, y int) int {
+	m := x * y
+	return m
 }
