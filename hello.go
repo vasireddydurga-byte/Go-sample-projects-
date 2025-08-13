e82d7c73db3ff44a8f66de95db7215a66aa1984a
vasireddydurga-byte <vasireddydurga@gmail.com>
2025-08-13 12:56:32 -0700

    Add files via upload

--- hello.go
+++ hello.go
@@ -1,13 +1,16 @@
 package main
-import ("myproject/simplecalc"
-"fmt"
+
+import (
+	"fmt"
+	"myproject/simplecalc"
 )
+
 func main() {
-	
+
 	fmt.Println("Hello World")
-a,b := 6,4
-fmt.Println(simplecalc.Add(a,b))
-c, d := 10, 3
+	a, b := 6, 4
+	fmt.Println(simplecalc.Add(a, b))
+	c, d := 10, 3
 	fmt.Println(simplecalc.Sub(c, d))
 
 	e, f := 20, 4
@@ -15,15 +18,15 @@ c, d := 10, 3
 
 	x, y := 30, 3
 	fmt.Println(simplecalc.Mul(x, y))
-num := 6
+	num := 6
 	if num > 6 {
 		fmt.Println("Number is greater than 6")
 	} else if num == 6 {
 		fmt.Println("Number is exactly 6")
 	} else {
 		fmt.Println("Number is less than 6")
-}
-day := "wednesday"
+	}
+	day := "wednesday"
 	switch day {
 	case "monday":
 		fmt.Println("Weekday")
@@ -35,5 +38,5 @@ day := "wednesday"
 	fmt.Println("Counting from 1 to 6:")
 	for i := 1; i <= 6; i++ {
 		fmt.Println(i)
-}
+	}
 }
