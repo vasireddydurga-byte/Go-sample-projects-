3dfeda0f224e50a44393a6f4be10af25cd2df7cf
vasireddydurga-byte <vasireddydurga@gmail.com>
2025-08-13 12:48:20 -0700

    Add files via upload

--- hello.go
+++ hello.go
@@ -15,5 +15,25 @@ c, d := 10, 3
 
 	x, y := 30, 3
 	fmt.Println(simplecalc.Mul(x, y))
-
+num := 6
+	if num > 6 {
+		fmt.Println("Number is greater than 6")
+	} else if num == 6 {
+		fmt.Println("Number is exactly 6")
+	} else {
+		fmt.Println("Number is less than 6")
+}
+day := "monday"
+	switch day {
+	case "monday":
+		fmt.Println("Weekday")
+	case "Saturday":
+		fmt.Println("Weekend")
+	default:
+		fmt.Println("It's another day")
+	}
+	fmt.Println("Counting from 1 to 6:")
+	for i := 1; i <= 6; i++ {
+		fmt.Println(i)
+}
 }
