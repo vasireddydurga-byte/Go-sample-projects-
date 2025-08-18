e931f524fcb4f9c12fb4d0324d0f23858bb786ed
vasireddydurga-byte <vasireddydurga@gmail.com>
2025-08-18 12:07:24 -0700

    Add files via upload

--- hello.go
+++ hello.go
@@ -5,9 +5,41 @@ import (
 	"myproject/simplecalc"
 )
 
-func main() {
+type address struct {
+	address1 string
+	address2 string
+	state    string
+	pin      int
+}
 
-	fmt.Println("Hello World")
+func main() {{
+
+
+	L:= make([]int,3 )
+	L = append(L,3)
+	L= append(L,4)
+	L = append(L,5)
+	L= append(L,6)
+	fmt.Println(len(L))
+	fmt.Println(cap(L))
+K :=[5] int{20,30,40,50, 60}
+slice := K[:4]
+fmt.Println(slice)
+}
+ 
+	D := address{
+		address1: "paseo laguna ",
+		address2: "livermore",
+		state:    "california",
+		pin:      94451,
+	}
+	fmt.Println(D.address1)
+	fmt.Println(D.address2)
+	fmt.Println(D.state)
+	fmt.Println(D.pin)
+	fmt.Println(D)
+
+fmt.Println("Hello World")
 	a, b := 6, 4
 	fmt.Println(simplecalc.Add(a, b))
 	c, d := 10, 3
@@ -39,4 +71,7 @@ func main() {
 	for i := 1; i <= 6; i++ {
 		fmt.Println(i)
 	}
+
 }
+
+
